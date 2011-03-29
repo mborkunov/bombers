1102347abf5aa09f6bcf17913f2085df747aea6a
maxim borkunov <maxim.borkunov@mborkunovwks.jlctech.com>
2011-03-29 21:13:06 +0400

    bomber default position was updated. also changed default color

--- js/game/objects/bomber.js
+++ js/game/objects/bomber.js
@@ -2,16 +2,17 @@ var Bomber = Class.create(GameObject, {
   controller: null,
   x: null,
   y: null,
-  initialize: function($super, controller) {
-    this.x = 0;
-    this.y = 0;
+  initialize: function($super, controller, color) {
+    this.x = 150;
+    this.y = 150;
+    this.color = color;
     controller.attach(this);
     this.controller = controller;
   },
   render: function(container) {
     if (!this.element && container) {
 
-      this.element = new Element("div").setStyle({width: "40px", height: "40px", background: "red", position: "absolute", top: "0", left: "0", zIndex: 100});
+      this.element = new Element("div").setStyle({width: "40px", height: "40px", background: this.color, position: "absolute", top: this.y + "px", left: this.x + "px", zIndex: 100});
       container.appendChild(this.element);
     }
   },
@@ -34,5 +35,6 @@ var Bomber = Class.create(GameObject, {
       top: this.y + "px",
       left: this.x + "px"
     });
-  }
+  },
+  spawnBomb: function() {}
 })
