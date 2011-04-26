6e713556682456358e0a41ff68cda62288b563fa
Energy <Energy@Power>
2011-04-27 01:17:40 +0400

    credits screen was created

--- js/game/credits.js
+++ js/game/credits.js
@@ -1,24 +1,84 @@
 var Credits = Class.create(Screen, {
-     counter: 0,
-     name: 'Credits',
-     menu: null,
-     rendered: false,
-     init: function() {
-         this.listeners = {
-             click: function(e) {
-             },
-             keydown: function(e) {
-               Game.instance.setScreen(Menu);
-             }
-         };
-     },
-     dispatch: function($super) {
-        $super();
-     },
-     update: function(delay) {
+  counter: 0,
+  name: 'Credits',
+  menu: null,
+  rendered: false,
+  text: null,
+  stop: null,
+  init: function() {
+    this.stop = false;
+    this.listeners = {
+      mousewheel: function(e) {
+        var delta = 5;
+        this.margin += e.wheelDeltaY < 0 ? - delta : delta;
+      }.bind(this),
+      mousedown: function(e) {
+        this.stop = true;
+      }.bind(this),
+      mouseup: function(e) {
+        this.stop = false;
+      }.bind(this),
+      keydown: function(e) {
+        if (!e.hasModifiers()) {
+          Game.instance.setScreen(Menu);
+        }
+      }
+    };
 
-     },
-     render: function(delay) {
+    this.margin = $('game-container').getHeight();
+    this.text = '<h1>ClanBomber2 Credits</h1><br/>' +
+        '<h2>Game Design</h2>' +
+        'Andreas Hundt<br/>' +
+        'Denis Oliver Kropp<br/>' +
+        '<br/>' +
+        '<h2>Coding</h2>' +
+        'Denis Oliver Kropp<br/>' +
+        'Andreas Hundt<br/>' +
+        'mass (network code and oza stuf)<br/>' +
+        '<br/>' +
+        '<h2>Graphics</h2>' +
+        'Andreeshchev Eugeni<br/>' +
+        'Denis Oliver Kropp<br/>' +
+        'Andreas Hundt<br/>' +
+        '<br/>' +
+        '<h2>Thanks go out to:</h2>' +
+        'Magnus Norddahl<br/>' +
+        '(24h ClanLib support)<br/>' +
+        'Fredrik Hallenberg<br/>' +
+        '(Debian package maintainer)<br/>' +
+        'non<br/>' +
+        '(for creating horst (the guy to the left))<br/>' +
+        'clanner and resix<br/>' +
+        '(for creating maps and playing with us)<br/>' +
+        'Ivar<br/>' +
+        '(for creating maps)<br/>' +
+        'Magnus Reftel<br/>' +
+        '(disable shaky explosions patch)<br/>' +
+        'the xtux creators<br/>' +
+        '(for the original tux and bsd-devil graphics)<br/>' +
+        'SuSE Linux AG<br/>' +
+        '(for donating free SuSE Linux Professional packages)<br/>' +
+        '<br/><br/>' +
+        'Everyone else supporting this game...<br/>' +
+        '... and playing it<br/>';
 
-     }
+    this.div = new Element('div').update(this.text).setStyle({
+      position: 'absolute',
+      marginTop: this.margin + "px"
+    });
+    this.container.appendChild(this.div);
+  },
+  dispatch: function($super) {
+    $super();
+  },
+  update: function(delay) {
+    if (Math.abs(this.margin) - 180 > $('game-container').getHeight()) {
+      Game.instance.setScreen(Menu);
+    }
+
+    if (!this.stop) this.margin -= 0.5;
+  },
+  render: function(delay) {
+    this.div.style.marginTop = parseInt(this.margin) + 'px';
+  }
 });
