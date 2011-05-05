ba01ede94083c2af4465930377d6590657d72e0e
Energy <Energy@Power>
2011-05-06 03:47:50 +0400

    cross image was added

--- js/util/common.js
+++ js/util/common.js
@@ -2,6 +2,6 @@ var date = function() {
   return new Date().getTime();
 };
 
-KeyboardEvent.prototype.hasModifiers = function() {
+(KeyboardEvent || Event).prototype.hasModifiers = function() {
   return (this.ctrlKey || this.altKey || this.shiftKey || this.metaKey);
 }
