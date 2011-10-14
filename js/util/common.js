d743a60721863cd0e3f1bb30de69b50bac54f78d
maxim borkunov <maxim.borkunov@mborkunovwks.jlctech.com>
2011-10-14 20:22:35 +0400

    scale and rotate fixes for opera were implemented

--- js/util/common.js
+++ js/util/common.js
@@ -2,10 +2,6 @@ var date = function() {
   return new Date().getTime();
 };
 
-(KeyboardEvent || Event).prototype.hasModifiers = function() {
-  return (this.ctrlKey || this.altKey || this.shiftKey || this.metaKey);
-};
-
 var Util = {
   iterate: function(items, func) {
     for (var i = 0, length = items.length; i < length; i++) {
@@ -20,6 +16,8 @@ Element.addMethods({
       element.style['-webkit-transform'] = 'rotate(' + angle +'deg) ';
     } else if (Prototype.Browser.Gecko) {
       element.style.MozTransform = 'rotate(' + angle +'deg) ';
+    } else if (Prototype.Browser.Opera) {
+      element.style.setProperty('-o-transform', 'rotate(' + angle +'deg)', false);
     } else {
       element.style['transform'] = 'rotate(' + angle +'deg) ';
     }
@@ -29,8 +27,14 @@ Element.addMethods({
     var y = scaleY || x;
     if (Prototype.Browser.WebKit) {
       element.style['-webkit-transform'] = 'scale(' + x + ', ' + y + ')';
+    } else if (Prototype.Browser.Opera) {
+      element.style.setProperty('-o-transform', 'scale(' + x + ', ' + y + ')', false);
     } else if (Prototype.Browser.Gecko) {
       element.style.MozTransform = 'scale(' + x + ', ' + y + ')';
     }
   }
 });
+
+(typeof(KeyboardEvent) == 'undefined' ? Event : KeyboardEvent).prototype.hasModifiers = function() {
+  return (this.ctrlKey || this.altKey || this.shiftKey || this.metaKey);
+};
